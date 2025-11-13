fde96032c0d083d03196f529b1d3caecf8db4a60
Rowan Border <rowanjborder@gmail.com>
2025-11-13 21:13:22 +0200

    add framerate

--- src/ffmpeg_publisher.cpp
+++ src/ffmpeg_publisher.cpp
@@ -19,91 +19,109 @@
 
 using namespace std::placeholders;
 
-namespace ffmpeg_image_transport
-{
+namespace ffmpeg_image_transport {
 using ParameterValue = ParameterDefinition::ParameterValue;
 using ParameterDescriptor = ParameterDefinition::ParameterDescriptor;
 using ParameterType = rcl_interfaces::msg::ParameterType;
 
 static const ParameterDefinition params[] = {
-  {ParameterValue("libx264"),
-   ParameterDescriptor()
-     .set__name("encoder")
-     .set__type(ParameterType::PARAMETER_STRING)
-     .set__description("ffmpeg encoder to use, see ffmpeg supported encoders")
-     .set__read_only(false),
-   ""},
-  {ParameterValue(""),
-   ParameterDescriptor()
-     .set__name("encoder_av_options")
-     .set__type(ParameterType::PARAMETER_STRING)
-     .set__description("comma-separated list of AV options: profile:main,preset:ll")
-     .set__read_only(false),
-   ""},
-  {ParameterValue(""),
-   ParameterDescriptor()
-     .set__name("pixel_format")
-     .set__type(ParameterType::PARAMETER_STRING)
-     .set__description("pixel format to use for encoding")
-     .set__read_only(false),
-   ""},
-  {ParameterValue(static_cast<int>(-1)),
-   ParameterDescriptor()
-     .set__name("qmax")
-     .set__type(ParameterType::PARAMETER_INTEGER)
-     .set__description("max video quantizer scale, see ffmpeg docs")
-     .set__read_only(false)
-     .set__integer_range(
-       {rcl_interfaces::msg::IntegerRange().set__from_value(-1).set__to_value(1024).set__step(1)}),
-   ""},
-  {ParameterValue(static_cast<int64_t>(-1)),
-   ParameterDescriptor()
-     .set__name("bit_rate")
-     .set__type(ParameterType::PARAMETER_INTEGER)
-     .set__description("target bit rate, see ffmpeg docs")
-     .set__read_only(false)
-     .set__integer_range({rcl_interfaces::msg::IntegerRange()
-                            .set__from_value(-1)
-                            .set__to_value(std::numeric_limits<int>::max())
-                            .set__step(1)}),
-   ""},
-  {ParameterValue(static_cast<int>(10)),
-   ParameterDescriptor()
-     .set__name("gop_size")
-     .set__type(ParameterType::PARAMETER_INTEGER)
-     .set__description("gop size (distance between keyframes)")
-     .set__read_only(false)
-     .set__integer_range({rcl_interfaces::msg::IntegerRange()
-                            .set__from_value(-1)
-                            .set__to_value(std::numeric_limits<int>::max())
-                            .set__step(1)}),
-   "gop_size not set, defaulting to 10. This may severely affect lag and performance!"},
-  {ParameterValue(static_cast<int>(0)),
-   ParameterDescriptor()
-     .set__name("max_b_frames")
-     .set__type(ParameterType::PARAMETER_INTEGER)
-     .set__description("max number of b frames")
-     .set__read_only(false)
-     .set__integer_range({rcl_interfaces::msg::IntegerRange()
-                            .set__from_value(0)
-                            .set__to_value(std::numeric_limits<int>::max())
-                            .set__step(1)}),
-   ""},
-  {ParameterValue(false),
-   ParameterDescriptor()
-     .set__name("encoder_measure_performance")
-     .set__type(ParameterType::PARAMETER_BOOL)
-     .set__description("enable performance timing")
-     .set__read_only(false),
-   ""},
+    {ParameterValue("libx264"),
+     ParameterDescriptor()
+         .set__name("encoder")
+         .set__type(ParameterType::PARAMETER_STRING)
+         .set__description(
+             "ffmpeg encoder to use, see ffmpeg supported encoders")
+         .set__read_only(false),
+     ""},
+    {ParameterValue(""),
+     ParameterDescriptor()
+         .set__name("encoder_av_options")
+         .set__type(ParameterType::PARAMETER_STRING)
+         .set__description(
+             "comma-separated list of AV options: profile:main,preset:ll")
+         .set__read_only(false),
+     ""},
+    {ParameterValue(""),
+     ParameterDescriptor()
+         .set__name("pixel_format")
+         .set__type(ParameterType::PARAMETER_STRING)
+         .set__description("pixel format to use for encoding")
+         .set__read_only(false),
+     ""},
+    {ParameterValue(static_cast<int>(-1)),
+     ParameterDescriptor()
+         .set__name("qmax")
+         .set__type(ParameterType::PARAMETER_INTEGER)
+         .set__description("max video quantizer scale, see ffmpeg docs")
+         .set__read_only(false)
+         .set__integer_range({rcl_interfaces::msg::IntegerRange()
+                                  .set__from_value(-1)
+                                  .set__to_value(1024)
+                                  .set__step(1)}),
+     ""},
+    {ParameterValue(static_cast<int64_t>(-1)),
+     ParameterDescriptor()
+         .set__name("bit_rate")
+         .set__type(ParameterType::PARAMETER_INTEGER)
+         .set__description("target bit rate, see ffmpeg docs")
+         .set__read_only(false)
+         .set__integer_range(
+             {rcl_interfaces::msg::IntegerRange()
+                  .set__from_value(-1)
+                  .set__to_value(std::numeric_limits<int>::max())
+                  .set__step(1)}),
+     ""},
+    {ParameterValue(static_cast<int>(10)),
+     ParameterDescriptor()
+         .set__name("gop_size")
+         .set__type(ParameterType::PARAMETER_INTEGER)
+         .set__description("gop size (distance between keyframes)")
+         .set__read_only(false)
+         .set__integer_range(
+             {rcl_interfaces::msg::IntegerRange()
+                  .set__from_value(-1)
+                  .set__to_value(std::numeric_limits<int>::max())
+                  .set__step(1)}),
+     "gop_size not set, defaulting to 10. This may severely affect lag and "
+     "performance!"},
+    {ParameterValue(static_cast<int>(30)),
+     ParameterDescriptor()
+         .set__name("frame_rate")
+         .set__type(ParameterType::PARAMETER_INTEGER)
+         .set__description("frame rate")
+         .set__read_only(false)
+         .set__integer_range({rcl_interfaces::msg::IntegerRange()
+                                  .set__from_value(0)
+                                  .set__to_value(60)
+                                  .set__step(1)}),
+     ""},
+    {ParameterValue(static_cast<int>(0)),
+     ParameterDescriptor()
+         .set__name("max_b_frames")
+         .set__type(ParameterType::PARAMETER_INTEGER)
+         .set__description("max number of b frames")
+         .set__read_only(false)
+         .set__integer_range(
+             {rcl_interfaces::msg::IntegerRange()
+                  .set__from_value(0)
+                  .set__to_value(std::numeric_limits<int>::max())
+                  .set__step(1)}),
+     ""},
+    {ParameterValue(false),
+     ParameterDescriptor()
+         .set__name("encoder_measure_performance")
+         .set__type(ParameterType::PARAMETER_BOOL)
+         .set__description("enable performance timing")
+         .set__read_only(false),
+     ""},
 };
 
-FFMPEGPublisher::FFMPEGPublisher() : logger_(rclcpp::get_logger("FFMPEGPublisher")) {}
+FFMPEGPublisher::FFMPEGPublisher()
+    : logger_(rclcpp::get_logger("FFMPEGPublisher")) {}
 
 FFMPEGPublisher::~FFMPEGPublisher() {}
 
-void FFMPEGPublisher::shutdown()
-{
+void FFMPEGPublisher::shutdown() {
   if (encoder_.isInitialized()) {
     RCLCPP_INFO_STREAM(logger_, "flushing encoder.");
     encoder_.flush();
@@ -112,14 +130,15 @@ void FFMPEGPublisher::shutdown()
 
 // This code was lifted from compressed_image_transport
 
-void FFMPEGPublisher::declareParameter(NodeType node, const ParameterDefinition & definition)
-{
+void FFMPEGPublisher::declareParameter(NodeType node,
+                                       const ParameterDefinition& definition) {
   // transport scoped parameter (e.g. image_raw.compressed.format)
   const auto v = definition.declare(node, paramNamespace_);
-  const auto & n = definition.descriptor.name;
+  const auto& n = definition.descriptor.name;
   if (n == "encoder") {
     encoder_.setEncoder(v.get<std::string>());
-    RCLCPP_INFO_STREAM(logger_, "using libav encoder: " << v.get<std::string>());
+    RCLCPP_INFO_STREAM(logger_,
+                       "using libav encoder: " << v.get<std::string>());
   } else if (n == "encoder_av_options") {
     handleAVOptions(v.get<std::string>());
   } else if (n == "pixel_format") {
@@ -132,6 +151,8 @@ void FFMPEGPublisher::declareParameter(NodeType node, const ParameterDefinition 
     encoder_.setBitRate(v.get<int>());
   } else if (n == "gop_size") {
     encoder_.setGOPSize(v.get<int>());
+  } else if (n == "frame_rate") {
+    encoder_.setFrameRate(v.get<int>(), 1);
   } else if (n == "encoder_measure_performance") {
     measurePerformance_ = v.get<bool>();
     encoder_.setMeasurePerformance(v.get<bool>());
@@ -140,24 +161,25 @@ void FFMPEGPublisher::declareParameter(NodeType node, const ParameterDefinition 
   }
 }
 
-void FFMPEGPublisher::handleAVOptions(const std::string & opt)
-{
+void FFMPEGPublisher::handleAVOptions(const std::string& opt) {
   const auto split = utils::splitAVOptions(opt);
-  for (const auto & sl : split) {
+  for (const auto& sl : split) {
     const auto kv = utils::splitAVOption(sl);
     if (kv.size() != 2) {
       RCLCPP_WARN_STREAM(logger_, "skipping bad AV option: " << sl);
     } else {
       encoder_.addAVOption(kv[0], kv[1]);
-      RCLCPP_INFO_STREAM(logger_, "setting AV option " << kv[0] << " to " << kv[1]);
+      RCLCPP_INFO_STREAM(logger_,
+                         "setting AV option " << kv[0] << " to " << kv[1]);
     }
   }
 }
 
-void FFMPEGPublisher::packetReady(
-  const std::string & frame_id, const rclcpp::Time & stamp, const std::string & codec,
-  uint32_t width, uint32_t height, uint64_t pts, uint8_t flags, uint8_t * data, size_t sz)
-{
+void FFMPEGPublisher::packetReady(const std::string& frame_id,
+                                  const rclcpp::Time& stamp,
+                                  const std::string& codec, uint32_t width,
+                                  uint32_t height, uint64_t pts, uint8_t flags,
+                                  uint8_t* data, size_t sz) {
   auto msg = std::make_shared<FFMPEGPacket>();
   msg->header.frame_id = frame_id;
   msg->header.stamp = stamp;
@@ -175,27 +197,28 @@ void FFMPEGPublisher::packetReady(
 }
 
 #ifdef IMAGE_TRANSPORT_NEEDS_PUBLISHEROPTIONS
-void FFMPEGPublisher::advertiseImpl(
-  NodeType node, const std::string & base_topic, QoSType custom_qos, rclcpp::PublisherOptions opt)
-{
+void FFMPEGPublisher::advertiseImpl(NodeType node,
+                                    const std::string& base_topic,
+                                    QoSType custom_qos,
+                                    rclcpp::PublisherOptions opt) {
   auto qos = initialize(node, base_topic, custom_qos);
   FFMPEGPublisherPlugin::advertiseImpl(node, base_topic, qos, opt);
 }
 #else
-void FFMPEGPublisher::advertiseImpl(
-  NodeType node, const std::string & base_topic, rmw_qos_profile_t custom_qos)
-{
+void FFMPEGPublisher::advertiseImpl(NodeType node,
+                                    const std::string& base_topic,
+                                    rmw_qos_profile_t custom_qos) {
   auto qos = initialize(node, base_topic, custom_qos);
   FFMPEGPublisherPlugin::advertiseImpl(node, base_topic, qos);
 }
 #endif
 
 FFMPEGPublisher::QoSType FFMPEGPublisher::initialize(
-  NodeType node, const std::string & base_topic, QoSType custom_qos)
-{
+    NodeType node, const std::string& base_topic, QoSType custom_qos) {
   // namespace handling code lifted from compressed_image_transport
 #ifdef IMAGE_TRANSPORT_USE_NODEINTERFACE
-  uint ns_len = std::string(node.get_node_base_interface()->get_namespace()).length();
+  uint ns_len =
+      std::string(node.get_node_base_interface()->get_namespace()).length();
 #else
   uint ns_len = node->get_effective_namespace().length();
 #endif
@@ -206,28 +229,30 @@ FFMPEGPublisher::QoSType FFMPEGPublisher::initialize(
   std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
   paramNamespace_ = param_base_name + "." + getTransportName() + ".";
 
-  for (const auto & p : params) {
+  for (const auto& p : params) {
     declareParameter(node, p);
   }
   // bump queue size to 2 * distance between keyframes
 #ifdef IMAGE_TRANSPORT_USE_QOS
   custom_qos.keep_last(
-    std::max(static_cast<int>(custom_qos.get_rmw_qos_profile().depth), 2 * encoder_.getGOPSize()));
+      std::max(static_cast<int>(custom_qos.get_rmw_qos_profile().depth),
+               2 * encoder_.getGOPSize()));
 #else
-  custom_qos.depth = std::max(static_cast<int>(custom_qos.depth), 2 * encoder_.getGOPSize());
+  custom_qos.depth =
+      std::max(static_cast<int>(custom_qos.depth), 2 * encoder_.getGOPSize());
 #endif
   return (custom_qos);
 }
 
-void FFMPEGPublisher::publish(const Image & msg, const PublisherTFn & publisher) const
-{
-  FFMPEGPublisher * me = const_cast<FFMPEGPublisher *>(this);
+void FFMPEGPublisher::publish(const Image& msg,
+                              const PublisherTFn& publisher) const {
+  FFMPEGPublisher* me = const_cast<FFMPEGPublisher*>(this);
   me->publishFunction_ = &publisher;
   if (!me->encoder_.isInitialized()) {
-    if (!me->encoder_.initialize(
-          msg.width, msg.height,
-          std::bind(&FFMPEGPublisher::packetReady, me, _1, _2, _3, _4, _5, _6, _7, _8, _9),
-          msg.encoding)) {
+    if (!me->encoder_.initialize(msg.width, msg.height,
+                                 std::bind(&FFMPEGPublisher::packetReady, me,
+                                           _1, _2, _3, _4, _5, _6, _7, _8, _9),
+                                 msg.encoding)) {
       RCLCPP_ERROR_STREAM(logger_, "cannot initialize encoder");
       return;
     }
